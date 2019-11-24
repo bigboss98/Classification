a635ecaa78da7a08339ec1fdd0d1ad9088ab7b2f
bigboss98 <marco_natali@ymail.com>
2019-11-24 17:42:47 +0100

    Add files via upload

--- Classification.py
+++ Classification.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import datasets
+import sklearn
 import pandas as pd
 from word2number import w2n
 import nltk
@@ -11,6 +11,15 @@ def data_exploration():
     print("Frequency of Values: ")
     print(dataset['POS'].copy().value_counts())
 
+    for item in range(0, len(dataset['Word']) - 1):
+        try:
+            if w2n.word_to_num(dataset['Word'][item]):
+               value = w2n.word_to_num(dataset['Word'][item])
+               dataset['Word'][item] = str(value)
+        except ValueError:
+            pass
+
+
 
     patterns = [
         (r'.*ing', 'VBG'),
@@ -19,11 +28,13 @@ def data_exploration():
         (r'.*s', 'NNS'),
         (r'.*uld', 'MD'),
         (r'.*y', 'RB'),
+        (r'^-?[0-9]+(\.[0-9]+)?$', 'CD'),
+        (r'.*est', 'JJS'),
         (r'.*', 'NN')
     ]
 
     rule_tagger = nltk.RegexpTagger(patterns)
-    print(rule_tagger.tag(dataset['Word'][:50]))
-    print("Accuracy, ", rule_tagger.evaluate((dataset['Word'], dataset['POS'])))
+    solutions = rule_tagger.tag(dataset['Word'])
+    print("Accuracy, ", sklearn.metrics.accuracy_score(dataset['POS'], [x for (_, x) in solutions]))
 
 data_exploration()
